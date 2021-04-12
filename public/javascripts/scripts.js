0aae2f885b89759ddc9965687792d98d3baab77c
Faith Bacci <faithbacci@aol.com>
2021-04-11 20:56:31 -0400

    Using jQuery to manipulate the DOM

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     console.log("success");
 })
+
+
+function displayTitle(name) {
+    let title = $('#title');
+    title.append($(`<title>Fair Fashion</Title`));
+} 
