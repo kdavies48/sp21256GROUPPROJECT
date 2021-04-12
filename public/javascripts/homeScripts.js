b37de60eba61f5bb3d68de21f9e4b62e3123cfd8
Faith Bacci <faithbacci@aol.com>
2021-04-11 22:05:02 -0400

    Added prompt for user to sign up for emails

--- public/javascripts/homeScripts.js
+++ public/javascripts/homeScripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     console.log("success");
     changeColor();
-})
+    addEmail();
+});
 
 function displayTitle(name) {
     let title = $('#title');
@@ -14,4 +15,15 @@ function changeColor() {
     console.log("color changed to red");
     title.style.setProperty("color", "black");
     console.log("color changed back to black!");
+}
+
+function addEmail() {
+    var email = prompt("Please enter your email for 25% off your next purchase!");
+    var emptyString = "";
+    if (email != null) {
+        console.log(email);
+        alert("Thank you! Check your inbox for your discount code!");
+    } else if(email === emptyString || email === null) {
+        console.log("no email provided by user");
+    }
 }
