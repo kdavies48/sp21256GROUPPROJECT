f46f2b163ff3cbba36d26d353f05ac9add981e94
kdavies48 <kaelynn@thedavies.us>
2021-04-27 16:11:19 -0400

    moved routes and updated CRUD again

---
+++ app/models/note.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'); //copied from https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/ 
+
+const NoteSchema = mongoose.Schema({
+    name: {
+        desc: "The member's name",
+        trim: true,
+        type: String,
+        required: true
+    },
+    birthday:{
+        desc:"The member's birthday",
+        trim:true,
+        type: Date,
+        required: true,
+    },
+    email:{
+        desc: "The member's email",
+        trim:true,
+        type: String,
+        required: true
+    },
+    password:{
+        desc:"The member's password",
+        trim:true,
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
